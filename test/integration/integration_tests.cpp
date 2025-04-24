2262a38a7a422078b392cd4e1977770ec8be306e
yangxiangmin <yang.xiangmin@ghtchina.com>
2025-04-24 14:21:26 +0800

    modify file integration_tests.cpp

--- test/integration/integration_tests.cpp
+++ test/integration/integration_tests.cpp
@@ -4,6 +4,7 @@
 
 std::atomic<bool> message_received(false);
 std::atomic<bool> fanout_received(false);
+std::atomic<bool> report_received(false);
 
 void topicCallback(const std::string& routing_key, const std::string& message, 
                  const std::string& sender, const std::string& trans) {
@@ -12,12 +13,24 @@ void topicCallback(const std::string& routing_key, const std::string& message,
     }
 }
 
-void fanoutCallback(const std::string& routing_key, const std::string& message,
-                  const std::string& sender, const std::string& trans) {
+void ReportEventHandler(
+    const char *serverName,
+    const char *srcMod,
+    const char *msgName,
+    const char *pJsonBody)
+{
+    if (msgName == "p2p-report-msg") {
+        report_received = true;
+    }
+}
+
+#if 0
+void fanoutCallback(const std::string& message, const int_props_st& intprops) {
     if (message == "fanout message") {
         fanout_received = true;
     }
 }
+#endif
 
 TEST(RabbitMQIntegrationTest, TopicMessageExchange) {
     RabbitMQWrapper& pub_wrapper = RabbitMQWrapper::getInstance(topictype);
@@ -43,6 +56,28 @@ TEST(RabbitMQIntegrationTest, TopicMessageExchange) {
     sub_wrapper.rabbitmq_free();
 }
 
+TEST(RabbitMQIntegrationTest, P2PMessageExchange) {
+    int rslt = rmInitMidwareClient("mod_p2ptest", "localhost", 5672, "guest", "guest", false);
+
+    MID_CLIENT_HANDLER stClientCBHandle = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
+
+    stClientCBHandle.m_cbReport = ReportEventHandler;
+    
+    rmSetMidMsgHandler(&stClientCBHandle);
+
+    rslt = rmReportMake("mod_p2ptest", "p2p-report-msg", "message body");
+
+    // 等待消息接收
+    for (int i = 0; i < 10 && !report_received; ++i) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    
+    EXPECT_TRUE(report_received);
+
+    MidwareClientDestroy();
+}
+
+#if 0
 TEST(RabbitMQIntegrationTest, FanoutMessageExchange) {
     RabbitMQWrapper& wrapper1 = RabbitMQWrapper::getInstance(broadcasttype);
     wrapper1.rabbitmq_init("localhost", 5672, "guest", "guest", "test_fanout1");
@@ -63,3 +98,4 @@ TEST(RabbitMQIntegrationTest, FanoutMessageExchange) {
     wrapper1.rabbitmq_free();
     wrapper2.rabbitmq_free();
 }
+#endif
